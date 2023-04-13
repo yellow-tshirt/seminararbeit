0c2c8345ec8ba21d4d4f64958a168604d86b6b98
mario <mario.occhinegro@abusix.com>
2023-04-13 14:55:57 +0200

    updated rust

--- RustExamples/src/main.rs
+++ RustExamples/src/main.rs
@@ -1,5 +1,5 @@
 fn main(){
-    let cm: Message = Message::Color(255, 255, 255);
+    let cm: Message = Message::Color(50, 50, 50);
     processMessage(cm);
 
 }
@@ -8,6 +8,7 @@ fn processMessage(m:Message){
     match m{
         Message::Hello => println!("Hello Message received -> Say Hello"),
         Message::Text(t) => println!("Text Message received -> Text was {}", t),
+        Message::Color(1,2,3) => println!("That's a weird color"),
         Message::Color(r,g,b) => println!("Color Message received -> Color was {} {} {}", r,g,b)
     }
 
