50680cf2e5270572bd866793e2faf5d0cd1a3ce7
mario <mario.occhinegro@abusix.com>
2023-04-29 16:41:31 +0200

    more examples

---
+++ java/Interface/interfaces.java
@@ -0,0 +1,27 @@
+package java.Interface;
+
+public class interfaces {
+    public static void main(String[] args) {
+        
+    }
+}
+
+interface Shape{
+    public int area();
+}
+class Square implements Shape{
+    public int x;
+    @Override
+    public int area() {
+        return x*x;
+    }
+}
+
+class Rectangle implements Shape{
+    public int x;
+    public int y;
+    @Override
+    public int area() {
+        return x*y;
+    }
+}
--- rust/Traits/src/main.rs
+++ rust/Traits/src/main.rs
@@ -1,4 +1,44 @@
 fn main() {
-    println!("Hello, world!");
+    let s = Square{a: 10};
+    print!("{}", s.area())
 }
+trait Shape{
+    fn area(s: &Self) ->i32;
+}
+
+trait ShapeS{
+    fn area(&self) -> i32;
+}
+
+impl ShapeS for Square{
+    fn area(&self) -> i32{
+        self.a*self.a
+    }
+}
+
+
+struct Square{
+    a: i32
+}
+
+impl Shape for Square {
+    fn area(s: &Self)->i32{
+        s.a*s.a
+    }
+}
+
+struct Rectangle{
+    a: i32,
+    b: i32
+}
+
+impl Shape for Rectangle {
+    fn area(s: &Self)->i32{
+        s.a*s.b
+    }
+}
+
+fn sum_area<A:Shape,B:Shape>(x : &A, y : &B) -> i32 {
+    return Shape::area(x) + Shape::area(y)
+ }
 
