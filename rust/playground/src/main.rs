03d14027d2c262cde4f1e477e0a979bd61aeb4c7
mario <mario.occhinegro@abusix.com>
2023-05-30 16:28:23 +0200

    more code, changed structure, more examples

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,3 +1,46 @@
-fn main(){
+fn main() {
+    let s = HskaStudent{name:"Mario", university:"hska", fav_language:"rust", git_username:"yellow-tshirt"};
+    comp_sci_student_greeting(&s);
+}
+trait Person {
+    fn name(&self) -> String;
+}
+trait Student: Person {
+    fn university(&self) -> String;
+}
+trait Programmer {
+    fn fav_language(&self) -> String;
+}
+trait CompSciStudent: Programmer + Student {
+    fn git_username(&self) -> String;
+}
+fn comp_sci_student_greeting<S: CompSciStudent>(student: &S) {
+    println!("Hey my name is {}, I study at {}. My favorite language is {} and my git username is {}", student.name(), student.university(), student. fav_language(), student.git_username());
+}
+struct HskaStudent{
+    name: &'static str,
+    university: &'static str,
+    fav_language: &'static str,
+    git_username: &'static str,
+}
+impl Person for HskaStudent{
+    fn name(&self) -> String{
+        self.name.to_string()
+    }
+}
+impl Student for HskaStudent{
+    fn university(&self) -> String {
+        String::from(self.university)
+    }
 }
 
+impl Programmer for HskaStudent{
+    fn fav_language(&self) -> String{
+        String::from(self.fav_language)
+    }
+}
+impl CompSciStudent for HskaStudent{
+    fn git_username(&self) -> String {
+        String::from(self.git_username)
+    }
+}
