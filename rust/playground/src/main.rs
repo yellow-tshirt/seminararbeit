287e97e3a8e9a8982fa166b7288e767434c17f56
mario <mario.occhinegro@abusix.com>
2023-05-30 17:32:41 +0200

    added some code examples

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,46 +1,26 @@
 fn main() {
-    let s = HskaStudent{name:"Mario", university:"hska", fav_language:"rust", git_username:"yellow-tshirt"};
-    comp_sci_student_greeting(&s);
-}
-trait Person {
-    fn name(&self) -> String;
-}
-trait Student: Person {
-    fn university(&self) -> String;
-}
-trait Programmer {
-    fn fav_language(&self) -> String;
-}
-trait CompSciStudent: Programmer + Student {
-    fn git_username(&self) -> String;
-}
-fn comp_sci_student_greeting<S: CompSciStudent>(student: &S) {
-    println!("Hey my name is {}, I study at {}. My favorite language is {} and my git username is {}", student.name(), student.university(), student. fav_language(), student.git_username());
-}
-struct HskaStudent{
-    name: &'static str,
-    university: &'static str,
-    fav_language: &'static str,
-    git_username: &'static str,
-}
-impl Person for HskaStudent{
-    fn name(&self) -> String{
-        self.name.to_string()
-    }
-}
-impl Student for HskaStudent{
-    fn university(&self) -> String {
-        String::from(self.university)
-    }
+   let s = Square{a: 10};
+   print!("{}", Shape2::area(&s));
 }
 
-impl Programmer for HskaStudent{
-    fn fav_language(&self) -> String{
-        String::from(self.fav_language)
-    }
+struct Square{
+    a: u32,
 }
-impl CompSciStudent for HskaStudent{
-    fn git_username(&self) -> String {
-        String::from(self.git_username)
-    }
+trait Shape{
+   fn area(&self) -> u32;
+}
+
+impl Shape for Square{
+   fn area(&self) -> u32{
+       self.a*self.a
+   }
+}
+trait Shape2{
+   fn area(&self) -> u32;
+}
+
+impl Shape2 for Square{
+   fn area(&self) -> u32{
+       self.a*self.a
+   }
 }
