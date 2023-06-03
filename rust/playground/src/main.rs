dec6d52327d6f8a00551cfc2a058a8f636926897
mario <mario.occhinegro@abusix.com>
2023-06-03 16:50:05 +0200

    added summary, added influence by haskell and some more descriptions

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,17 +1,13 @@
-use std::thread;
-use std::time::Duration;
-
-fn main() {
-    thiryparty_struct{}.sleep();
-}
-struct thiryparty_struct{}
-
-trait Sleep{
-    fn sleep(&self);
-}
-
-impl Sleep for thiryparty_struct {
-    fn sleep(&self){
-        thread::sleep(Duration::from_millis(1000));
-    }
+pub enum Exp {
+    Int {
+        val: i32
+    },
+    Plus {
+        left: Box<Exp>,
+        right: Box<Exp>
+    },
+    Mult{
+        left: impl Exp,
+        right: impl Exp
+    },
 }
