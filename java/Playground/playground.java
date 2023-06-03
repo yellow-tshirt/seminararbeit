d966fb3ec15fa6436b5cf4a7c38a028aea40cfc4
mario <mario.occhinegro@abusix.com>
2023-06-03 18:18:48 +0200

    update

--- java/Playground/playground.java
+++ java/Playground/playground.java
@@ -1,8 +1,18 @@
 public class playground{
 	public static void main(String[] args) {
-		
+		Animal e1 = Animal.Bird;
+		Animal e2 = Animal.Bird;
 	}
 	
 }
+enum Animal{
+    Dog,
+    Cat,
+    Bird;
 
-
+    public boolean isCat(){
+        if (this == Animal.Cat){
+            return true;
+        }
+    }
+}
