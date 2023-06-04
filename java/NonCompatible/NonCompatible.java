71494e5ef7d97cb83b51b9de33811e1caa52d4d7
mario <mario.occhinegro@abusix.com>
2023-06-04 15:02:49 +0200

    fast fertig - sleeep pls

--- java/NonCompatible/NonCompatible.java
+++ java/NonCompatible/NonCompatible.java
@@ -1,13 +1,17 @@
 public class NonCompatible{
     public static void main(String[] args) {
-        
     }
 }
 
-class SomeClass implements musicplayer, boardgame{
-    public void play(){
-        System.out.println("You are playing");
+class NonCompatibleClass implements musicplayer, boardgame{
+    public NonCompatibleClass(){
+
     }
+
+	@Override
+	public void play() {
+        System.out.println("Is playing");
+	}
 }
 interface musicplayer{
     public void play();
--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,13 +1,26 @@
-pub enum Exp {
-    Int {
-        val: i32
-    },
-    Plus {
-        left: Box<Exp>,
-        right: Box<Exp>
-    },
-    Mult{
-        left: impl Exp,
-        right: impl Exp
-    },
+fn main(){
+    let m = Machine{};
+    let a: i8 = 16;
+    let b: Vec<i32> = TransformAB::transform(&m, a);
+}
+trait TransformAB{
+    type A;
+    type B;
+    const TIMES: u8;
+    fn transform(s: &Self, a: Self::A) -> Vec<Self::B>;
+}
+
+struct Machine{}
+impl TransformAB for Machine{
+    type A = i8;
+    type B = i32;
+    const TIMES:u8 = 50;
+    fn transform(s: &Self, a: Self::A) -> Vec<Self::B>{
+        let mut v = Vec::new();
+        let a32 = i32::from(a);
+        for i in 0..Self::TIMES  {
+            v.push(a32);
+        }
+        v
+    }
 }
