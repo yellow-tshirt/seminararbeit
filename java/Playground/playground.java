44f1bbd58afe672713496e1ac177de215fd6d48a
mario <mario.occhinegro@abusix.com>
2023-06-04 13:59:03 +0200

    Added more regarding switch case + examples

--- java/Playground/playground.java
+++ java/Playground/playground.java
@@ -1,18 +1,23 @@
-public class playground{
-	public static void main(String[] args) {
-		Animal e1 = Animal.Bird;
-		Animal e2 = Animal.Bird;
-	}
-	
+public class playground {
+    public static void main(String[] args) {
+        Animal a = Animal.Bird;
+        switch (a){
+            case Dog:
+                System.out.println("It's a Dog");
+                break;
+            case Bird:
+                System.out.println("It's a Bird");
+                break;
+            case Cat:
+                System.out.println("It's a Dog");
+                break;
+            default:
+                System.out.println("This Animal wasn't expected");
+        }
+    }
 }
 enum Animal{
     Dog,
     Cat,
-    Bird;
-
-    public boolean isCat(){
-        if (this == Animal.Cat){
-            return true;
-        }
-    }
+    Bird
 }
